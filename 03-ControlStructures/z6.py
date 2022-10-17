a2921b702d99b53b72537b8b66a8e3972dd117be
s-121-18 <s-121-18@gumiforest.ae.krakow.pl>
2022-10-17 13:51:37 +0200

    lekcja

---
+++ 03-ControlStructures/z6.py
@@ -0,0 +1,5 @@
+s = int(input("enter car speed "))
+if s > 130:
+    print("too fast")
+else:
+    print("ok")
---
+++ 03-ControlStructures/z9.py
@@ -0,0 +1,7 @@
+a = int(input("enter num a "))
+b = int(input("enter num b"))
+
+if a > 0 or b > 0:
+    print("at least one is positive")
+else:
+    print("neither is positive")
